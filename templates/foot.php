0b23931b6143baf0d4062c8750776938a966eb25
Maaax128 <maaax128@list.ru>
2019-03-13 23:53:45 +0500

    правка ошибок

--- templates/foot.php
+++ templates/foot.php
@@ -1,5 +1,5 @@
-<script src="../js/jquery-2.1.1.js"></script>
-<script src="../js/jquery.mobile.custom.min.js"></script>
-<script src="../js/main.js"></script> <!-- Resource jQuery -->
+<script src="js/jquery-2.1.1.js"></script>
+<script src="js/jquery.mobile.custom.min.js"></script>
+<script src="js/main.js"></script> <!-- Resource jQuery -->
 </body>
 </html>
