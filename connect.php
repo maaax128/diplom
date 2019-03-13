6a044f8f8f98512200e88ed304d2a51df0cc42ca
Maaax128 <maaax128@list.ru>
2019-03-13 23:40:02 +0500

    правки

--- connect.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$pdo = new PDO("mysql:host=localhost;charset=UTF8; dbname=diplom", "root","");
-//$pdo = new PDO("mysql:host=localhost;charset=UTF8; dbname=diplom", "root","985958");
-//$pdo = new PDO("mysql:host=localhost;charset=UTF8; dbname=mmaltsev", "mmaltsev","neto1891");
-?>
