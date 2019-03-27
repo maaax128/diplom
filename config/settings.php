2a21718ac0ab17557a5107f85795935d3c1548dc
Maaax128 <maaax128@list.ru>
2019-03-27 22:35:22 +0500

    правка ошибок

---
+++ config/settings.php
@@ -0,0 +1,26 @@
+<?php
+
+//подключение к базе
+return array(
+    'settings' => array(
+        'displayErrorDetails' => true
+    ),
+    'db' => array(
+        'driver'    => 'mysql',
+        'host'      => 'localhost',
+        'dbname'    => 'mmaltsev',
+        'user'      => 'mmaltsev',
+        'pass'      => 'neto1891',
+        'charset'   => 'utf8',
+        'collation' => 'utf8_unicode_ci',
+    ),
+    'local_db' => array(
+        'driver'    => 'mysql',
+        'host'      => 'localhost',
+        'dbname'    => 'diplom',
+        'user'      => 'root',
+        'pass'      => '',
+        'charset'   => 'utf8',
+        'collation' => 'utf8_unicode_ci',
+    )
+);
---
+++ controller/innerController.php
@@ -0,0 +1,69 @@
+<?php
+if (empty($_POST)) {
+	die();
+}
+    require('model/Connect.php');
+    require('model/Questions.php');
+    require('model/Users.php');
+    require('model/Answers.php');
+
+    $model = new questions();
+    $model->newConnect();
+
+    $users = new users();
+    $users->newConnect();
+
+    $answers = new answers();
+    $answers->newConnect();
+
+    switch ($_POST['method']) {
+        case 'deleteAdmin':
+            $users->DeleteAdmins($_POST['adminId']);
+            header("Location:controlPanel.php");
+            break;
+
+        case 'addNewADmins':
+            $var = array("name"=>$_POST['name'],
+                "password"=>$_POST['password'],
+
+            );
+            $users->addNewAdmin($var);
+            header("Location:controlPanel.php");
+            break;
+
+        case 'changePassword':
+            $users->changePassword($_POST);
+           header("Location:controlPanel.php");
+            break;
+
+        case 'addCategory':
+            $model->addCategory($_POST['categoryName']);
+            header("Location:controlPanel.php");
+            break;
+
+        case 'deleteCategory':
+            $model->deleteCategory($_POST['categoryId']);
+            header("Location:controlPanel.php");
+            break;
+
+        case 'deleteQuestion':
+            $model->deleteQuestion($_POST['questionId']);
+            header("Location:controlPanel.php");
+            break;
+
+        case 'hideQuestion':
+            $model->hideQuestion($_POST['questionId'], $_POST['status']);
+            header("Location:controlPanel.php");
+            break;
+
+        case 'editQuestion':
+            $model->editQuestion($_POST);
+            header("Location:controlPanel.php");
+            break;
+
+        case 'addAnswer':
+            $answers->addAnswer($_POST);
+            header("Location:controlPanel.php");
+            break;
+    }
+
